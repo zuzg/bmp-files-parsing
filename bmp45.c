f5c950cd591d6a8c8ea6fd2836de7862e0eb9e26
zuzg <81712088+zuzg@users.noreply.github.com>
2021-04-19 10:30:04 +0200

    works now!!!

--- bmp45.c
+++ bmp45.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <stdint.h>
 #include <math.h>
+#pragma warning(disable:4996)
+
 
 typedef uint16_t WORD;
 typedef uint32_t DWORD;
@@ -10,29 +12,29 @@ typedef int32_t LONG;
 
 // https://docs.microsoft.com/en-us/windows/win32/api/wingdi/ns-wingdi-bitmapfileheader
 typedef struct tagBITMAPFILEHEADER {
-  WORD  bfType;
-  DWORD bfSize;
-  WORD  bfReserved1;
-  WORD  bfReserved2;
-  DWORD bfOffBits;
-} BITMAPFILEHEADER, *LPBITMAPFILEHEADER, *PBITMAPFILEHEADER;
+    WORD  bfType;
+    DWORD bfSize;
+    WORD  bfReserved1;
+    WORD  bfReserved2;
+    DWORD bfOffBits;
+} BITMAPFILEHEADER, * LPBITMAPFILEHEADER, * PBITMAPFILEHEADER;
 
 // https://docs.microsoft.com/pl-pl/previous-versions/dd183376(v=vs.85)
 typedef struct tagBITMAPINFOHEADER {
-  DWORD biSize;
-  LONG  biWidth;
-  LONG  biHeight;
-  WORD  biPlanes;
-  WORD  biBitCount;
-  DWORD biCompression;
-  DWORD biSizeImage;
-  LONG  biXPelsPerMeter;
-  LONG  biYPelsPerMeter;
-  DWORD biClrUsed;
-  DWORD biClrImportant;
-} BITMAPINFOHEADER, *LPBITMAPINFOHEADER, *PBITMAPINFOHEADER;
+    DWORD biSize;
+    LONG  biWidth;
+    LONG  biHeight;
+    WORD  biPlanes;
+    WORD  biBitCount;
+    DWORD biCompression;
+    DWORD biSizeImage;
+    LONG  biXPelsPerMeter;
+    LONG  biYPelsPerMeter;
+    DWORD biClrUsed;
+    DWORD biClrImportant;
+} BITMAPINFOHEADER, * LPBITMAPINFOHEADER, * PBITMAPINFOHEADER;
 
-void readfile(BITMAPFILEHEADER *fh, FILE *fp){
+void readfile(BITMAPFILEHEADER* fh, FILE* fp) {
     fread(&fh->bfType, sizeof fh->bfType, 1, fp);
     fread(&fh->bfSize, sizeof fh->bfSize, 1, fp);
     fread(&fh->bfReserved1, sizeof fh->bfReserved1, 1, fp);
@@ -40,7 +42,7 @@ void readfile(BITMAPFILEHEADER *fh, FILE *fp){
     fread(&fh->bfOffBits, sizeof fh->bfOffBits, 1, fp);
 }
 
-void readinfo(BITMAPINFOHEADER *ih, FILE *fp){
+void readinfo(BITMAPINFOHEADER* ih, FILE* fp) {
     fread(&ih->biSize, sizeof ih->biSize, 1, fp);
     fread(&ih->biWidth, sizeof ih->biWidth, 1, fp);
     fread(&ih->biHeight, sizeof ih->biHeight, 1, fp);
@@ -54,34 +56,45 @@ void readinfo(BITMAPINFOHEADER *ih, FILE *fp){
     fread(&ih->biClrImportant, sizeof ih->biClrImportant, 1, fp);
 }
 
-void printhistogram(int *arr, float max){
-    for(int i = 0; i < 16; i++)
-        printf("%u-%u: %.2f%%\n", i*16, 16*i+15, arr[i] / max);
+void printhistogram(int* arr, float max) {
+    for (int i = 0; i < 16; i++)
+        printf("%u-%u: %.2f%%\n", i * 16, 16 * i + 15, arr[i] / max);
 }
 
 
-//troche bez sensu zrobilam bo potrzebujemy potem tych tablic r g b
-void histogram(int *red, int *green, int *blue, FILE *fp, BITMAPFILEHEADER *fh, BITMAPINFOHEADER *ih, int rowlength){
-    if(ih->biCompression != 0 || ih->biBitCount != 24){
+void histogram(int* redB, int* greenB, int* blueB, FILE* fp, BITMAPFILEHEADER* fh, BITMAPINFOHEADER* ih, int rowlength) {
+    if (ih->biCompression != 0 || ih->biBitCount != 24) {
         printf("\nhistogram calculation is unsupported");
         return;
     }
 
+    int red[16] = { 0 };
+    int green[16] = { 0 };
+    int blue[16] = { 0 };
+
     uint8_t r;
     uint8_t g;
     uint8_t b;
+    int k = 0;
 
-    for(int j = 0; j < ih->biHeight; j++){
-        for(int i = 0; i < rowlength; i += 3){
+    for (int j = 0; j < ih->biHeight; j++) {
+        for (int i = 0; i < rowlength; i ++) {
             fread(&r, sizeof r, 1, fp);
             fread(&g, sizeof g, 1, fp);
             fread(&b, sizeof b, 1, fp);
 
-            //printf("%u", r);
+            //printf("%u %u %u ", r, g, b);
+            redB[k] = r;
+            greenB[k] = g;
+            blueB[k] = b;
+            //printf("%d %d %d\n", r, g, b);
+            //printf("%d %d %d\n", redB[k], greenB[k], blueB[k]);
+            k++;
 
-            red[r/16]++;
-            green[g/16]++;
-            blue[b/16]++;
+
+            red[r / 16]++;
+            green[g / 16]++;
+            blue[b / 16]++;
         }
     }
     printf("\nRED\n");
@@ -93,10 +106,10 @@ void histogram(int *red, int *green, int *blue, FILE *fp, BITMAPFILEHEADER *fh, 
 
 }
 
-//cos nie styka, format niby git ale plik sie nie otwiera
-void tograyscale(int *red, int *green, int *blue, char *filename, BITMAPFILEHEADER *fh, BITMAPINFOHEADER *ih, int rowlength){
-    FILE *fp = fopen(filename, "wb");
-    if(!fp) {
+
+void tograyscale(int* redB, int* greenB, int* blueB, char* filename, BITMAPFILEHEADER* fh, BITMAPINFOHEADER* ih, int rowlength) {
+    FILE* fp = fopen(filename, "wb");
+    if (!fp) {
         perror("File opening failed");
         return EXIT_FAILURE;
     }
@@ -121,25 +134,26 @@ void tograyscale(int *red, int *green, int *blue, char *filename, BITMAPFILEHEAD
 
 
     uint8_t gray;
-    //uint8_t padding = 0;
+    int k = 0;
 
-    for(int j = 0; j < ih->biHeight; j++){
-        for(int i = 0; i < rowlength; i += 3){
-            gray = (red[i] + green[i] + blue[i]) / 3;
-            //printf("%d ", gray);
+    for (int j = 0; j < ih->biHeight; j++) {
+        for (int i = 0; i < rowlength; i ++) {
+            gray = (redB[k] + greenB[k] + blueB[k]) / 3;
+            //gray = (uint8_t)temp;
+            //printf("%d %d %d %d\n", gray, redB[i], greenB[i], blueB[i]);
             fwrite(&gray, sizeof gray, 1, fp);
             fwrite(&gray, sizeof gray, 1, fp);
             fwrite(&gray, sizeof gray, 1, fp);
+            k++;
         }
-        //fwrite(padding, sizeof padding, 1, fp);
     }
-  fclose(fp);
+    fclose(fp);
 }
 
-int main(){
+int main() {
 
-    FILE *fp = fopen("tux.bmp", "rb");
-    if(!fp) {
+    FILE* fp = fopen("tux.bmp", "rb");
+    if (!fp) {
         perror("File opening failed");
         return EXIT_FAILURE;
     }
@@ -153,28 +167,34 @@ int main(){
 
     printf("BITMAPFILEHEADER\n");
     printf("bfType: 0x%X\nbfSize: %u\nbfReserved1: 0x%X\nbfReserved2: 0x%X\nbfOffBits: %u\n",
-           fh.bfType, fh.bfSize, fh.bfReserved1, fh.bfReserved2, fh.bfOffBits);
+        fh.bfType, fh.bfSize, fh.bfReserved1, fh.bfReserved2, fh.bfOffBits);
 
     printf("\nBITMAPINFOHEADER\n");
     printf("biSize: %u\nbiWidth: %d\nbiHeight: %d\nbiPlanes: %u\nbiBitCount: %u\nbiCompression: %u\n",
-           ih.biSize, ih.biWidth, ih.biHeight, ih.biPlanes, ih.biBitCount, ih.biCompression);
+        ih.biSize, ih.biWidth, ih.biHeight, ih.biPlanes, ih.biBitCount, ih.biCompression);
     printf("biSizeImage: %u\nbiXPelsPerMeter: %u\nbiYPelsPerMeter: %u\nbiClrUsed: %u\nbiClrImportant: %u\n",
-           ih.biSizeImage, ih.biXPelsPerMeter, ih.biYPelsPerMeter, ih.biClrUsed, ih.biClrImportant);
+        ih.biSizeImage, ih.biXPelsPerMeter, ih.biYPelsPerMeter, ih.biClrUsed, ih.biClrImportant);
 
 
 
     int rowlength = floor((ih.biBitCount * ih.biWidth + 31) / 32) * 4;
     //printf("%d", rowlength);
 
-    int red[16] = { 0 };
-    int green[16] = { 0 };
-    int blue[16] = { 0 };
+    int el = rowlength * ih.biHeight;
 
-    histogram(red, green, blue, fp, &fh, &ih, rowlength);
-
-    tograyscale(red, green, blue, "sadtux.bmp", &fh, &ih, rowlength);
+    int* redB = (int*)calloc(el, sizeof(int));
+    int* greenB = (int*)calloc(el, sizeof(int));
+    int* blueB = (int*)calloc(el, sizeof(int));
 
 
+    histogram(redB, greenB, blueB, fp, &fh, &ih, rowlength);
+
+
+    tograyscale(redB, greenB, blueB, "tuxnotsosad.bmp", &fh, &ih, rowlength);
+
+    free(redB);
+    free(greenB);
+    free(blueB);
     fclose(fp);
     return 0;
 }
