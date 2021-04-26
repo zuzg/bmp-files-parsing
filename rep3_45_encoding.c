882dddc57866a63c174ae8ad0864ec25a89d1159
azywot <82370491+azywot@users.noreply.github.com>
2021-04-26 13:47:38 +0200

    Create rep3_45_encoding.c

---
+++ rep3_45_encoding.c
@@ -0,0 +1,50 @@
+#include "header.h"
+//#include "header2.h"
+
+int main() {
+
+    char * filename = "tux.bmp";
+
+    FILE* fp = fopen(filename, "rb");
+    if (!fp) {
+        perror("File opening failed");
+        return EXIT_FAILURE;
+    }
+
+    BITMAPFILEHEADER fh;
+    readfile(&fh, fp);
+
+    BITMAPINFOHEADER ih;
+    readinfo(&ih, fp);
+
+    int offset_size = fh.bfOffBits-14-sizeof(BITMAPINFOHEADER);
+    uint8_t * offset = (uint8_t *)calloc(offset_size, sizeof(uint8_t));
+    fread(offset,sizeof(uint8_t),offset_size,fp);
+
+    print_all(fh, ih);
+
+    int rowlength = floor((ih.biBitCount * ih.biWidth + 31) / 32) * 4;
+    int el = ih.biWidth * ih.biHeight;
+
+    COLORS ImgCol;
+    ImgCol.RED = (int*)calloc(el, sizeof(int));
+    ImgCol.GREEN = (int*)calloc(el, sizeof(int));
+    ImgCol.BLUE = (int*)calloc(el, sizeof(int));
+
+    histogram(&ImgCol, fp, &fh, &ih, rowlength);
+    tograyscale(&ImgCol, "TEST.bmp", &fh, &ih, rowlength, offset);
+
+    char * text = "Hello!";
+    steganography(text, filename, &ImgCol, &fh, &ih, offset);
+    //printf("Decode steganography? [Y/N] ");
+    //int ch = getc(stdin);
+    //if (ch == 'Y' || ch == 'y')
+       // printf("\nin progress...\n");
+
+
+    free(ImgCol.RED);
+    free(ImgCol.GREEN);
+    free(ImgCol.BLUE);
+    fclose(fp);
+    return 0;
+}
